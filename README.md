17d3b9522565ec24d747ba2a148375d6da82576b
teohwj <wjoo96@gmail.com>
2019-10-14 18:15:30 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+DS18B20 Temperature Sensor
+==========================
+A working code for ds18b20 temperature sensor in rpi. 
+
+Author
+------
+
+* Teoh Wooi Joo // wjoo96@gmail.com
+
+(c) 2019. 
---
+++ ds18b20.js
@@ -0,0 +1,35 @@
+//init
+var sensor = require ('ds18b20');
+var isLoaded = sensor.isDriverloaded();
+var io = require ('socket.io')(http);
+
+//redis init
+var redis = require('redis');
+var client = redis.createClient({host: 'localhost' , port: '6379'});
+//redis ready
+client.on("ready", function(){
+	console.log("Redis is ready");
+})
+// error handling
+client.on("error", function(err) {
+	console.log("Err: " + err);
+});
+
+
+//identify sensor
+if (isLoaded){
+	var deviceID = sensor.list();
+		console.log("Device found: " + deviceID);
+	if(deviceID.length == 0){
+		console.log("No device is found." + "Exiting...");
+		process.exit();
+	}
+	//get temperature data from sensor
+	sensor.temperature(deviceID, function(err,data){
+		console.log("The temperature is " + data + "C", data);
+	});
+
+} else {
+	console.log("device is not loaded!");
+};
+
