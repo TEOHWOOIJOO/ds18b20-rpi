b86a60e2db57be2cc2220732fe19baea5661bc5f
teohwj <wjoo96@gmail.com>
2019-10-22 00:45:32 +0800

    version 4 code

--- v4.js
+++ v4.js
@@ -5,10 +5,9 @@ var sensorId = '28-03219779933a ';
 	function writeMsg(res){
 		sensor.get(sensorId, function (err, temp){
 			if(err){
-				console.log(err);
-				
-			}else{
-				console.log("the temp is: " + temp ,temp);
+				console.log("error" + err);
+				return;	
 			}
+			res.write("the temp is: " + temp + '\n\n');
 		});	
 	}
