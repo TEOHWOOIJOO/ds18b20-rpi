d7e6b1151b8ba5ee33b5d2599dc578ea90a60633
teohwj <wjoo96@gmail.com>
2019-10-21 23:47:44 +0800

    new version code ds18b20(v2)

--- ds18b20(v2).js
+++ ds18b20(v2).js
@@ -7,18 +7,18 @@ function readTemp(callback){
 	fs.readFile('/sys/bus/w1/devices/28-03219779933a/w1_slave',function(err,buffer){
 		if(err){
 			console.error(err);
-			process.exit;
+			process.exit(1);
 		}
 
 	//read data from file
 	var data = buffer.toString('ascii').split(" "); //split bus space
 
 	//extract temp from string
-	var temp = parseFloat(data[data.length-1].split("=")[1])/1000.0;
+	var temp = parseFloat(data[data.length - 1].split("=")[1])/1000.0;
 
 	//rounding decimal
 	temp = Math.round(temp * 10) / 10;
 
 		callback(data);
-	})
-}
+	});
+};
