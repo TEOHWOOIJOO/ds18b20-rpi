9fe7968099bdc001ffc04169f813ab2aad379dd9
teohwj <wjoo96@gmail.com>
2019-10-24 17:36:02 +0800

    workable code for ds18b20 sensor

--- sensor.js
+++ sensor.js
@@ -1,8 +1,8 @@
 var ds18 = require ('ds18b20');
 var id = '';
-
 var redis = require('redis');
 var client = redis.createClient({host: 'localhost' , port: '6379'});
+
 //redis ready
 client.on("ready", function(){
 	console.log("Redis is ready");
@@ -11,11 +11,13 @@ client.on("ready", function(){
 client.on("error", function(err) {
 	console.log("Err: " + err);
 });
-
+//get sensor ID
 ds18.sensors(function (err, ids){
 	console.log ("Ids: " + ids);
 	id = ids;
 });
+
+//get data reading from the sensor
 setInterval(function(){
 	ds18.temperature('28-03219779933a', function(err, value){
 		data = value;
